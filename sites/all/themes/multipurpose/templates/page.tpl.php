ec14384b2b2bc36dd08df86b06a87a9cd560e5f2
Alexandr Klepikov <alexandr.klepikov@aira.cz>
2017-02-04 16:55:14 +0100

    Second

--- sites/all/themes/multipurpose/templates/page.tpl.php
+++ sites/all/themes/multipurpose/templates/page.tpl.php
@@ -1,52 +1,57 @@
 <div id="wrap">
+  <div id="header-wraper">
+    <div id="header-wrap">
+      <div id="header-wrap-inner">
+        <header id="header">
+          <div id="site-info">
+            <?php if ($logo): ?>
+              <div id="site-logo">
+                <?php
+                $logo = base_path() . drupal_get_path('theme', 'multipurpose') . '/images/logo-70.svg';
+                $logo_fall = base_path() . drupal_get_path('theme', 'multipurpose') . '/images/logo-70.png';
+                ?>
+                <a href="<?php print $front_page; ?>" title="<?php print $site_name; ?>">
+                  <img src="<?php print $logo; ?>" alt="<?php print $site_name; ?>"  onerror="this.src='<?php print $logo_fall; ?>'; this.onerror=null;" />
+                </a>
+              </div>
+            <?php endif; ?>
+            <?php if ($site_name || $site_slogan): ?>
+              <div id="site-name-slogan">
+                <?php if ($site_name): ?>
+                  <div id="site-name">
+                    <a href="<?php print $front_page; ?>" title="<?php print $site_name; ?>">
+                      <?php print $site_name; ?>
+                    </a>
+                  </div>
+                <?php endif; ?>
+                <?php if ($site_slogan): ?>
+                  <div id="site-slogan">
+                    <?php print $site_slogan; ?>
+                  </div>
+                <?php endif; ?>
+              </div>
+            <?php endif; ?>
+            <div class="clearfix"></div>
+          </div>
 
-  <div id="header-wrap">
-    <div id="header-wrap-inner">
-      <header id="header">
-        <div id="site-info">
-          <?php if ($logo): ?>
-            <div id="site-logo">
-              <a href="<?php print $front_page; ?>" title="<?php print $site_name; ?>">
-                <img src="<?php print $logo; ?>" alt="<?php print $site_name; ?>" />
-              </a>
-            </div>
-          <?php endif; ?>
-          <?php if ($site_name || $site_slogan): ?>
-            <div id="site-name-slogan">
-              <?php if ($site_name): ?>
-                <div id="site-name">
-                  <a href="<?php print $front_page; ?>" title="<?php print $site_name; ?>">
-                    <?php print $site_name; ?>
-                  </a>
-                </div>
-              <?php endif; ?>
-              <?php if ($site_slogan): ?>
-                <div id="site-slogan">
-                  <?php print $site_slogan; ?>
-                </div>
-              <?php endif; ?>
-            </div>
-          <?php endif; ?>
+          <div id="site-navigation-wrap">
+            <div id="sidr-close"><a href="#sidr-close" class="toggle-sidr-close"></a></div>
+            <a href="#sidr-main" id="navigation-toggle">
+              <span class="fa fa-bars"></span>Menu
+            </a>
+            <nav id="site-navigation" role="navigation">
+              <div id="main-menu">
+                <?php
+                $main_menu_tree = menu_tree(variable_get('menu_main_links_source', 'main-menu'));
+                print drupal_render($main_menu_tree);
+                ?>
+                <div class="clearfix"></div>
+              </div>
+            </nav>
+          </div>
           <div class="clearfix"></div>
-        </div>
-
-        <div id="site-navigation-wrap">
-          <div id="sidr-close"><a href="#sidr-close" class="toggle-sidr-close"></a></div>
-          <a href="#sidr-main" id="navigation-toggle">
-            <span class="fa fa-bars"></span>Menu
-          </a>
-          <nav id="site-navigation" role="navigation">
-            <div id="main-menu">
-              <?php
-              $main_menu_tree = menu_tree(variable_get('menu_main_links_source', 'main-menu'));
-              print drupal_render($main_menu_tree);
-              ?>
-              <div class="clearfix"></div>
-            </div>
-          </nav>
-        </div>
-        <div class="clearfix"></div>
-      </header>
+        </header>
+      </div>
     </div>
   </div>
 
@@ -200,6 +205,14 @@
           <div id="footer-block-wrap" class="clr">
             <?php if ($page['footer_first']): ?><div class="span_1_of_3 col col-1 footer-block ">
               <?php print render($page['footer_first']); ?>
+              </div><?php endif; ?>
+            <?php if ($page['footer_second']): ?><div class="span_1_of_3 col col-2 footer-block ">
+              <?php print render($page['footer_second']); ?>
+              </div><?php endif; ?>
+            <div class="span_1_of_3 col col-3 footer-block ">
+              <?php if ($page['footer_third']): ?>
+                <?php print render($page['footer_third']); ?>
+              <?php endif; ?>
               <?php if (theme_get_setting('socialicon_display', 'multipurpose')): ?>
                 <?php
                 $twitter_url = check_plain(theme_get_setting('twitter_url', 'multipurpose'));
@@ -207,31 +220,25 @@
                 $youtube_url = check_plain(theme_get_setting('youtube_url', 'multipurpose'));
                 $instagram_url = check_plain(theme_get_setting('instagram_url', 'multipurpose'));
                 ?>
-                  <div id="footer-social">
-                    <div class="footer-social-share">Najdete nás na:</div>
-                    <ul>
-                      <?php if ($facebook_url): ?><li>
-                          <a target="_blank" title="<?php print $site_name; ?> na Facebooku" href="<?php print $facebook_url; ?>"><img alt="Facebook" src="<?php print base_path() . drupal_get_path('theme', 'multipurpose') . '/images/social/black/facebook.svg'; ?>"> </a>
-                        </li><?php endif; ?>
-                      <?php if ($twitter_url): ?><li>
-                          <a target="_blank" title="<?php print $site_name; ?> na Twitteru" href="<?php print $twitter_url; ?>"><img alt="Twitter" src="<?php print base_path() . drupal_get_path('theme', 'multipurpose') . '/images/social/black/twitter.svg'; ?>"> </a>
-                        </li><?php endif; ?>
-                      <?php if ($youtube_url): ?><li>
-                          <a target="_blank" title="<?php print $site_name; ?> na Youtube" href="<?php print $youtube_url; ?>"><img alt="Google+" src="<?php print base_path() . drupal_get_path('theme', 'multipurpose') . '/images/social/black/youtube.svg'; ?>"> </a>
-                        </li><?php endif; ?>
-                      <?php if ($instagram_url): ?><li>
-                          <a target="_blank" title="<?php print $site_name; ?> na Instagramu" href="<?php print $instagram_url; ?>"><img alt="Pinterest" src="<?php print base_path() . drupal_get_path('theme', 'multipurpose') . '/images/social/black/instagram.svg'; ?>"> </a>
-                        </li><?php endif; ?>
-                    </ul>
-                  </div>
-                <?php endif; ?>
-              </div><?php endif; ?>
-            <?php if ($page['footer_second']): ?><div class="span_1_of_3 col col-2 footer-block ">
-              <?php print render($page['footer_second']); ?>
-              </div><?php endif; ?>
-            <?php if ($page['footer_third']): ?><div class="span_1_of_3 col col-3 footer-block ">
-              <?php print render($page['footer_third']); ?>
-              </div><?php endif; ?>
+                <div id="footer-social">
+                  <h2>Kde nás najdete?</h2>
+                  <ul>
+                    <?php if ($facebook_url): ?><li>
+                        <a target="_blank" title="<?php print $site_name; ?> na Facebooku" href="<?php print $facebook_url; ?>"><img alt="Facebook" src="<?php print base_path() . drupal_get_path('theme', 'multipurpose') . '/images/social/black/facebook.svg'; ?>"> </a>
+                      </li><?php endif; ?>
+                    <?php if ($twitter_url): ?><li>
+                        <a target="_blank" title="<?php print $site_name; ?> na Twitteru" href="<?php print $twitter_url; ?>"><img alt="Twitter" src="<?php print base_path() . drupal_get_path('theme', 'multipurpose') . '/images/social/black/twitter.svg'; ?>"> </a>
+                      </li><?php endif; ?>
+                    <?php if ($youtube_url): ?><li>
+                        <a target="_blank" title="<?php print $site_name; ?> na Youtube" href="<?php print $youtube_url; ?>"><img alt="Google+" src="<?php print base_path() . drupal_get_path('theme', 'multipurpose') . '/images/social/black/youtube.svg'; ?>"> </a>
+                      </li><?php endif; ?>
+                    <?php if ($instagram_url): ?><li>
+                        <a target="_blank" title="<?php print $site_name; ?> na Instagramu" href="<?php print $instagram_url; ?>"><img alt="Pinterest" src="<?php print base_path() . drupal_get_path('theme', 'multipurpose') . '/images/social/black/instagram.svg'; ?>"> </a>
+                      </li><?php endif; ?>
+                  </ul>
+                </div>
+              <?php endif; ?>
+            </div>
           </div>
         <?php endif; ?>
 
---
+++ sites/all/themes/multipurpose/templates/views-view-table--treninky--block-1.tpl.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ * Dolni brezany - content
+ */
+?>
+<?php
+$dayhastr = false;
+$days = array();
+$times = ['16:00', '16:30', '17:00', '17:30', '18:00', '18:30', '19:00', '19:30', '20:00', '20:30', '21:00', '21:30'];
+$total = sizeof($times);
+$count = 0;
+$col = 1;
+$prevcount = 0;
+?>
+<?php foreach ($rows as $row_count => $row): ?>
+  <?php
+  $days[$row['field_den_v_tydnu']][$row['nid']]['time'] = trim(strip_tags($row['field_cas_treninku']));
+  $days[$row['field_den_v_tydnu']][$row['nid']]['time_from'] = substr(trim(strip_tags($row['field_cas_treninku'])), 0, 5);
+  $days[$row['field_den_v_tydnu']][$row['nid']]['time_to'] = substr(trim(strip_tags($row['field_cas_treninku'])), -5);
+  $days[$row['field_den_v_tydnu']][$row['nid']]['urcen_pro'] = $row['field_urcen_pro_1'];
+  $days[$row['field_den_v_tydnu']][$row['nid']]['urcen_pro_key'] = $row['field_urcen_pro_2'];
+  $days[$row['field_den_v_tydnu']][$row['nid']]['place'] = $row['field_adresa_ref_2'];
+  ?>
+<?php endforeach; ?>
+<tr class="hide-mobile">
+  <?php foreach ($days as $day => $data): ?>
+    <td colspan="2"><?= $day; ?></td>
+    <?php foreach ($data as $nid => $training): ?>
+      <?php foreach ($times as $time): ?>
+
+        <?php $count++; ?>
+        <?php if ($time >= $training['time_from'] && $time <= $training['time_to']): ?>
+
+          <?php if ($time == $training['time_from']): ?>
+            <?php $col++; ?>
+            <?php if ($prevcount > 0): ?>
+              <?php $count = $count - $prevcount; ?>
+            <?php endif; ?>
+          <?php elseif ($time == $training['time_to']): ?>
+            <?php $prevcount = $count; ?>
+            <?php $col++; ?>
+            <?php $dayhastr = true; ?>
+
+            <?php if ($dayhastr === true): ?>
+              <td colspan="<?= $col; ?>" class="intended-for intended-for-<?= $training['urcen_pro_key']; ?>">
+                <div class="info">
+                  <div class="time"><?= $training['time']; ?><br><?= $training['place']; ?></div>
+                  <div class="intended-for"><?= $training['urcen_pro']; ?></div>
+                </div>
+              </td>
+              <?php $col = 1; ?>
+              <?php break; ?>
+            <?php else: ?>
+              <?php $dayhastr = false; ?>
+            <?php endif; ?>
+          <?php endif; ?>
+        <?php elseif ($dayhastr === false): ?>
+          <td></td>
+        <?php endif; ?>
+      <?php endforeach; ?>
+    <?php endforeach; ?>
+    <?php if ($count <= $total): ?>
+      <?php for ($i = $count; $i <= $total; $i++): ?>
+        <td></td>
+      <?php endfor; ?>
+    <?php endif; ?>
+  <?php endforeach; ?>
+</tr>
+
+<?php global $rowcount; ?>
+
+<?php foreach ($days as $day => $data): ?>
+  <?php foreach ($data as $nid => $training): ?>
+    <tr class="hide-wide <?php
+        if ($rowcount % 2 == 0) {
+          print 'odd';
+        }
+        else {
+          print 'even';
+        }
+        ?>">
+      <td><?= $day; ?></td>
+      <td>
+        <?php print $training['time']; ?> - <?php print $training['urcen_pro']; ?>
+      </td>
+    </tr>
+    <?php $rowcount++; ?>
+  <?php endforeach; ?>
+<?php endforeach; ?>
